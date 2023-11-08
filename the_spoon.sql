f4a820acf3a23dd00878d5fc4c45d4b4b9f40a72
Urgaar <82642331+Urgaar@users.noreply.github.com>
2023-11-08 09:49:08 +0100

    Bases de données the spoon
    BDD PHPmyadmin

---
+++ the_spoon.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 08 nov. 2023 à 09:44
+-- Version du serveur :  10.4.18-MariaDB
+-- Version de PHP : 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `the_spoon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `id_avis` int(11) NOT NULL,
+  `date_heure_avis` datetime NOT NULL,
+  `avis` text NOT NULL,
+  `id_individu` int(11) NOT NULL,
+  `id_restaurant` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `individu`
+--
+
+CREATE TABLE `individu` (
+  `id_individu` int(11) NOT NULL,
+  `nom` text NOT NULL,
+  `prenom` text NOT NULL,
+  `numero_tel` int(11) NOT NULL,
+  `role` text NOT NULL,
+  `adresse_mail` text NOT NULL,
+  `mot_de_passe` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id_reservation` int(11) NOT NULL,
+  `date_heure` datetime NOT NULL,
+  `nb_personne` int(11) NOT NULL,
+  `id_individu` int(11) NOT NULL,
+  `id_restaurant` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `id_restaurant` int(11) NOT NULL,
+  `description_restaurant` text NOT NULL,
+  `adresse_restaurant` text NOT NULL,
+  `ville_restaurant` text NOT NULL,
+  `code_postal_restaurant` text NOT NULL,
+  `livraison` tinyint(1) NOT NULL,
+  `horaires_ouverture` text NOT NULL,
+  `spécialite` text NOT NULL,
+  `note` decimal(10,0) NOT NULL,
+  `affluence_1` text NOT NULL,
+  `affluence_2` text NOT NULL,
+  `affluence_3` text NOT NULL,
+  `affluence_4` text NOT NULL,
+  `id_individu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD PRIMARY KEY (`id_avis`);
+
+--
+-- Index pour la table `individu`
+--
+ALTER TABLE `individu`
+  ADD PRIMARY KEY (`id_individu`);
+
+--
+-- Index pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id_reservation`);
+
+--
+-- Index pour la table `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`id_restaurant`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `avis`
+--
+ALTER TABLE `avis`
+  MODIFY `id_avis` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `individu`
+--
+ALTER TABLE `individu`
+  MODIFY `id_individu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id_reservation` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `id_restaurant` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
